29d89622b523f6a3fb7ee2deca4ed2c55f5eb63c
Luiz Oliveira <luizsoliveira@gmail.com>
2023-11-08 22:55:45 -0800

    Including header line

--- src/data_aggregation/merge_files.py
+++ src/data_aggregation/merge_files.py
@@ -1,10 +1,9 @@
 import fileinput
-import glob
 
-file_list = glob.glob("*.txt")
-
-def merge_files(files_in, file_out):
+def merge_files(files_in, file_out, header_line=False):
 
     with open(file_out, 'w') as file:
+        
         input_lines = fileinput.input(files_in)
+        if (header_line): file.write(f"{header_line}\n")
         file.writelines(input_lines)
