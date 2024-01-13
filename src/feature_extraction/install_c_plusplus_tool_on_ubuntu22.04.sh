757cf5a117c1d1b0ddca888d2f29a4a2bf253eb3
Luiz Oliveira <luizsoliveira@gmail.com>
2024-01-12 19:31:55 -0800

    updating scripts to install and update the mrtprocessor

--- src/feature_extraction/install_c_plusplus_tool_on_ubuntu22.04.sh
+++ src/feature_extraction/install_c_plusplus_tool_on_ubuntu22.04.sh
@@ -70,14 +70,15 @@ git checkout dev-2.0
 cd src/mrtprocessor/
 
 #Edit file CMakeLists.txt
-sed -i -e 's|/Users/ballanty/.local|/opt/bgp_research_tools/src/feature_extraction/mrtprocessor|g' ./CMakeLists.txt
+sed -i -e 's|$ENV{HOME}/.local|/opt/bgp_research_tools/src/feature_extraction/mrtprocessor|g' ./CMakeLists.txt
 
 #Edit file main.cpp
-sed -i -e 's|__u6_addr\.|__in6_u.|g' ./main.cpp
+# sed -i -e 's|__u6_addr\.|__in6_u.|g' ./main.cpp
 
 cmake .
 make
 ln -sf /opt/bgp_research_tools/src/feature_extraction/mrtprocessor/repo/src/mrtprocessor/mrtprocessor /opt/bgp_research_tools/src/feature_extraction/mrtprocessor/bin/mrtprocessor
+export DYLD_LIBRARY_PATH=/opt/bgp_research_tools/src/feature_extraction/mrtprocessor/lib
 /opt/bgp_research_tools/src/feature_extraction/mrtprocessor/bin/mrtprocessor --help
 
 
--- src/feature_extraction/update_c_plusplus_tool_on_ubuntu22.04.sh
+++ src/feature_extraction/update_c_plusplus_tool_on_ubuntu22.04.sh
@@ -2,8 +2,10 @@ cd /opt/bgp_research_tools/src/feature_extraction/mrtprocessor/repo/src/mrtproce
 git stash
 git pull
 #Edit file CMakeLists.txt
-sed -i -e 's|/Users/ballanty/.local|/opt/bgp_research_tools/src/feature_extraction/mrtprocessor|g' ./CMakeLists.txt
+# sed -i -e 's|/Users/ballanty/.local|/opt/bgp_research_tools/src/feature_extraction/mrtprocessor|g' ./CMakeLists.txt
+sed -i -e 's|$ENV{HOME}/.local|/opt/bgp_research_tools/src/feature_extraction/mrtprocessor|g' ./CMakeLists.txt
 cmake .
 make
 ln -sf /opt/bgp_research_tools/src/feature_extraction/mrtprocessor/repo/src/mrtprocessor/mrtprocessor /opt/bgp_research_tools/src/feature_extraction/mrtprocessor/bin/mrtprocessor
+export DYLD_LIBRARY_PATH=/opt/bgp_research_tools/src/feature_extraction/mrtprocessor/lib
 /opt/bgp_research_tools/src/feature_extraction/mrtprocessor/bin/mrtprocessor --help
