86ebfe68ae6aac9795c219f815d059338f676e60
Luiz Oliveira <luizsoliveira@gmail.com>
2024-01-26 14:01:15 -0800

    Data download functions CLI

---
+++ src/data_download/cli.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+import sys
+from datetime import datetime
+
+src_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(src_dir)
+
+from data_download.clients.ripe_client import RIPEClient
+
+def convert_datetime(date_time_input):
+    format="%Y%m%dT%H%M%S"
+    try:
+        return datetime.strptime(date_time_input, format)
+    except ValueError:
+        os.exit(f"ABORTING: ERROR. The input \"{date_time_input}\" does not match format {format}.")
+
+def data_download(datetime_start, datetime_end, rrc=4, site_collection='ripe', max_concurrent_requests=32):
+    client = RIPEClient(max_concurrent_requests=32)
+    return client.download_updates_interval_files(datetime_start,
+                                            datetime_end, int(rrc))
+    
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser('CLI for Download module of BGP Research Software')
+
+    parser.add_argument('--site-collection', dest='site_collection', default='ripe', choices=['ripe'], help='Choose a site collection that can be ripe or route-views (not available yet)')
+    parser.add_argument('--from', dest='datetime_start', type=str, required=True, help='Choose a datetime to start download in the format: yyyymmddThhmmss. Example: 20030521T080100 ')
+    parser.add_argument('--to', dest='datetime_end', type=str, required=True, help='Choose a datetime to start download in the format: yyyymmddThhmmss. Example: 20030521T080100 ')
+    parser.add_argument('--rrc', dest='rrc', type=int, default=4, help='Choose a RRC')
+    parser.add_argument('--max-concurrent-requests', dest='max_concurrent_requests', type=int, default=32, help='Choose a number of max concurrent requests')
+
+    args = parser.parse_args()
+
+    datetime_start = convert_datetime(args.datetime_start)
+    datetime_end = convert_datetime(args.datetime_end)
+
+    for file in data_download(datetime_start, datetime_end, args.rrc, args.site_collection, args.max_concurrent_requests):
+        print(file['file_path'])
+    
