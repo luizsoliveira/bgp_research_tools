32909d551bc0ad289dc5b3a1a07c2135f01d7924
Luiz Oliveira <luizsoliveira@gmail.com>
2024-02-10 19:19:00 -0800

    Cleaning data merging.

--- src/data_merging/cli.py
+++ src/data_merging/cli.py
@@ -8,7 +8,6 @@ src_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(src_dir)
 
 from data_merging.merge_files import *
-from data_labeling.anomalous_and_regular_data_labeling import AnomalousAndRegularDataLabeling
 
 HEADER_LINE = "POSIXTIME DATETIME HOUR MINUTE SECOND F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 F13 F14 F15 F16 F17 F18 F19 F20 F21 F22 F23 F24 F25 F26 F27 F28 F29 F30 F31 F32 F33 F34 F35 F36 F37"
 
@@ -33,11 +32,6 @@ if __name__ == "__main__":
     
     merge_files(files_in, args.output_filename, args.header_line)
 
-    data_labeler = AnomalousAndRegularDataLabeling()
-
-    data_labeler.new_dataset_with_labels(args.output_filename,args.output_filename, datetime.utcfromtimestamp(0), datetime.utcfromtimestamp(0))
-    print(f"Data labeling finished.")
-
-    print("Finished.")
+    print("Data merging finished. File written in {args.output_filename}.")
 
 
