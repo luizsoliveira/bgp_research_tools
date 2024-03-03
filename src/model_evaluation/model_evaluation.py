e07463705d39dd2a7eb3dfa67299a1656d720952
Luiz Oliveira <luizsoliveira@gmail.com>
2024-03-02 20:41:34 -0800

    Model evaluation functions and plot

---
+++ src/model_evaluation/model_evaluation.py
@@ -0,0 +1,109 @@
+# import torch
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score, f1_score, precision_score, recall_score
+import pandas as pd
+import numpy as np
+import os
+from model_evaluation.plots import plot_model_train_val_loss
+import json
+
+# This function can be called to evaluate the model against the train and test partitions
+# def univariate_autoencoder_evaluation_report(model, X, Y):
+
+#     X_tensor = torch.tensor(X, dtype=torch.float32)
+#     # Evaluate the autoencoder on test data
+#     model.eval()
+#     with torch.no_grad():
+#         reconstructed_data = model(X_tensor)
+#         reconstruction_loss = torch.mean((X_tensor - reconstructed_data)**2, dim=1).numpy()
+
+#     # Define a threshold for anomaly detection (e.g., based on the reconstruction error distribution)
+#     threshold = np.mean(reconstruction_loss) + 2 * np.std(reconstruction_loss)
+
+#     # Detect anomalies
+#     anomalies = X[reconstruction_loss > threshold]
+
+#     # Reconstruct X axis with X_ground_truth, X_predicted and anomaly flag
+#     df = pd.DataFrame(data = X, columns=['ground_truth_signal'])
+    
+#     # Just try to plot the reconstructed signal just when this signal has 2 dimensions
+#     # On the cases that LSTM is used and the reconstructed signal is as sequence
+#     # we have many reconstructed signals for the same data point in a way that the
+#     # sliding window is crossing
+#     if len(reconstructed_data) == 2:
+#         df['reconstructed_signal'] = reconstructed_data
+#     df['threshold'] = threshold
+#     df['reconstruction_loss'] = reconstruction_loss
+#     df['predicted_label'] = np.where(df['reconstruction_loss'] > threshold,1,0)
+#     df['prediction_score'] = np.where(df['predicted_label'] == Y,1,0)
+    
+#     return df, threshold
+
+def save_evaluation_details(location, model, history, x_test, y_test):
+    #Train and Test Loss
+    test_loss, test_acc = model.evaluate(x_test, y_test)
+    print("Test accuracy", test_acc)
+    print("Test loss", test_loss)
+    plot_train_val_los_filepath = os.path.join(location, "train_val_loss.png")
+    plot_model_train_val_loss(history, file_path=plot_train_val_los_filepath)
+
+    # Getting predictions
+    # For each data point returns a tuple with the
+    # probabilities to be class 0 (regular) or class 1 (anomalous)
+    Y_pred_prob = model.predict(x_test)
+
+    # Select the class with the max probability
+    # Arg max return the index of the element with the max value
+    # The axis=-1 parameter says to do this evaluation in the last dimension
+    y_pred  = np.argmax(Y_pred_prob, axis=-1)
+
+    # Checking shapes
+    if (len(y_pred) != len(y_test)):
+        raise Exception(f"Number of predictions different from number from y_test. Check the code.")
+    # if (len(y_pred) != len(dataset_test)):
+    #     raise Exception(f"Number of predictions different from dataset_test. Check the code.")
+    eval_metrics = calculate_metrics(y_test, y_pred, will_print=False)
+    metrics_filepath = os.path.join(location, "eval_metrics.json")
+    with open(metrics_filepath, "w") as metrics_file:
+        json.dump(eval_metrics, metrics_file, indent=4, default=str)
+
+    return y_pred
+
+def calculate_metrics(y_test, y_pred, will_print=True):
+
+    acc = accuracy_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+    precision = precision_score(y_test, y_pred, zero_division=np.nan)
+    recall = recall_score(y_test, y_pred, zero_division=np.nan)
+    report = classification_report(y_test, y_pred, output_dict=True, zero_division=np.nan)
+    report_txt = classification_report(y_test, y_pred, output_dict=False, zero_division=np.nan)
+
+    if (will_print):
+    #Calculating metrics
+        print("#"*60)
+        print(f"Accuracy: {acc}")
+        print(f"F1 score: {f1}")
+        print(f"Correct predictions: ")
+        print(f" TP: {tp}")
+        print(f" TN: {tn}")
+        print(f"Incorrect predictions: ")
+        print(f" FP: {fp}")
+        print(f" FN: {fn}")
+        print()
+        print(report_txt)
+        print("#"*60)
+
+    return dict({
+        "accuracy": acc,
+        "f1_score": f1,
+        "cf_matrix_tp": float(tp),
+        "cf_matrix_tn": float(tn),
+        "cf_matrix_fp": float(fp),
+        "cf_matrix_fn": float(fn),
+        "precision": precision,
+        "recall": precision,
+        "classification_report": report,
+    })
---
+++ src/model_evaluation/plots.py
@@ -0,0 +1,137 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.graph_objects as go
+import pandas as pd
+
+"""
+## Visualize curve samples
+
+Here we visualize one timeseries example for each class in the dataset.
+
+"""
+def plot_samples(x_train, y_train, y_test, columns):
+
+    classes = np.unique(np.concatenate((y_train, y_test), axis=0))
+
+    
+    plt.figure()
+
+    for n in range(1,4):
+
+        for c_idx, column in enumerate(columns):
+            plt.clf()
+            for c in classes:
+                c_x_train = x_train[y_train == c]
+                # Getting the N occurrence on the desired class
+                sample = c_x_train[0]
+                # Getting just the desired column from the time sequence 
+                sample = [s[c_idx] for s in sample]
+                plt.plot(sample, label=f"{column} class " + str(c))
+            
+            plt.legend(loc="best")
+            plt.title(f"Sample nº {n}/3 for column {column}")
+            plt.show()
+        
+        
+    plt.close()
+
+def plot_model_train_val_loss(history, file_path=False):
+    """
+    ## Plot the model's training and validation loss
+    """
+
+    metric = "sparse_categorical_accuracy"
+    plt.figure()
+    plt.plot(history.history[metric])
+    plt.plot(history.history["val_" + metric])
+    plt.title("model " + metric)
+    plt.ylabel(metric, fontsize="large")
+    plt.xlabel("epoch", fontsize="large")
+    plt.legend(["train", "val"], loc="best")
+    if (file_path):
+        plt.savefig(file_path)
+    else:
+        plt.show()
+    plt.close()
+
+
+def plot_overview_chart(dataset_train, dataset_test, y_pred, columns, title):
+
+    # Creating a local and independent copy
+    dataset_train = dataset_train.df.copy(deep=True)
+    dataset_test = dataset_test.df.copy(deep=True)
+
+    # Adjusting y_pred value just for plot
+    y_pred = list(map(lambda x: float(str(x).replace("1", "6.8")),y_pred))
+
+    dataset_test.loc[:,'y_pred'] = y_pred
+    dataset_train.loc[:,'is_train'] = -10
+    dataset_test.loc[:,'is_test'] = -10
+
+    graph_df = pd.concat([dataset_train,dataset_test], ignore_index=True)
+
+    graph_df['LABEL'] = list(map(lambda x: float(str(x).replace("1", "7")),graph_df['LABEL']))
+
+    traces = []
+
+    dates = graph_df['datetime'] if 'datetime' in graph_df.columns else (graph_df['DATETIME'] if 'DATETIME' in graph_df.columns else graph_df.index)
+
+    # Creating traces for the signals
+    for column in columns:
+        traces.append(
+            go.Scatter(
+            x = dates,
+            y = graph_df[column],
+            mode = 'lines',
+            line={'width': 0.75},
+            name = column,
+            visible='legendonly'
+            ))
+        print(f"column:  {column}, shape: {dataset_test[column].shape}")
+
+    traces.append(
+            go.Scatter(
+            x = dates,
+            y = graph_df['LABEL'],
+            mode = 'lines',
+            fill='tozeroy',
+            fillcolor='rgba(255,0,0,0.15)',
+            line={'width': 0},
+            name = 'Label'
+    ))
+    traces.append(go.Scatter(
+        x = dates,
+        y = graph_df['y_pred'],
+        mode = 'lines',
+        fill='tozeroy',
+        fillcolor='rgba(0,0,255,0.15)',
+        line={'width': 0},
+        name = 'Predicted label',
+    ))
+    traces.append(go.Scatter(
+        x = dates,
+        y = graph_df['is_train'],
+        mode = 'lines',
+        fill='tozeroy',
+        fillcolor='rgba(0,255,0,0.15)',
+        # fillpattern=dict({"shape":"."}),
+        line={'width': 0},
+        name = 'Train partition',
+    ))
+    traces.append(go.Scatter(
+        x = dates,
+        y = graph_df['is_test'],
+        mode = 'lines',
+        fill='tozeroy',
+        fillcolor='rgba(255,0,255,0.15)',
+        # fillpattern=dict({"shape":"."}),
+        line={'width': 0},
+        name = 'Test partition',
+    ))
+    layout = go.Layout(
+        title = title,
+        xaxis = {'title' : "Date"},
+        yaxis = {'title' : "Values"}
+    )
+    fig = go.Figure(data=traces, layout=layout)
+    fig.show()
