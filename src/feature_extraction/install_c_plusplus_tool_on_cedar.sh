a98e534f7414acfdd5e8c16e0afc253145026209
Luiz Oliveira <luizsoliveira@gmail.com>
2024-03-23 00:53:34 -0700

    Putting home shortcut on install script

--- src/feature_extraction/install_c_plusplus_tool_on_cedar.sh
+++ src/feature_extraction/install_c_plusplus_tool_on_cedar.sh
@@ -1,8 +1,9 @@
-mkdir /home/loa10/bgp_research_tools/src/feature_extraction/mrtprocessor
+mkdir ~/bgp_research_tools/src/feature_extraction/mrtprocessor
 cd /home/loa10/bgp_research_tools/src/feature_extraction/mrtprocessor 
 
 git clone https://github.com/RIPE-NCC/bgpdump tmp_bgpdump
 cd tmp_bgpdump
+
 autoheader
 autoconf
 ./configure --prefix /home/loa10/bgp_research_tools/src/feature_extraction/mrtprocessor
@@ -19,13 +20,15 @@ git checkout dev-2.0
 cd src/mrtprocessor/
 
 #Edit file CMakeLists.txt
-sed -i -e 's|$ENV{HOME}/.local|/home/loa10/bgp_research_tools/src/feature_extraction/mrtprocessor|g' ./CMakeLists.txt
+sed -i -e 's|$ENV{HOME}/.local|~/bgp_research_tools/src/feature_extraction/mrtprocessor|g' ./CMakeLists.txt
 sed -i -e 's|set(CMAKE_CXX_STANDARD 17)|set(CMAKE_CXX_STANDARD 20)|g' ./CMakeLists.txt
 #cmake .
 rm -fr CMakeFiles Makefile
-cmake -D CMAKE_C_COMPILER=/cvmfs/soft.computecanada.ca/gentoo/2020/usr/bin/gcc-11.3.0 -D CMAKE_CXX_COMPILER=/cvmfs/soft.computecanada.ca/gentoo/2020/usr/bin/g++-11.3.0 .
+cmake -D CMAKE_C_COMPILER=/cvmfs/soft.computecanada.ca/gentoo/2023/x86-64-v3/usr/bin/gcc-11 -D CMAKE_CXX_COMPILER=/cvmfs/soft.computecanada.ca/gentoo/2023/x86-64-v3/usr/bin/g++-11 .
 make
 
-ln -sf /home/loa10/bgp_research_tools/src/feature_extraction/mrtprocessor/repo/src/mrtprocessor/mrtprocessor /home/loa10/bgp_research_tools/src/feature_extraction/mrtprocessor/bin/mrtprocessor
-export DYLD_LIBRARY_PATH=/home/loa10/bgp_research_tools/src/feature_extraction/mrtprocessor/lib
-/home/loa10/bgp_research_tools/src/feature_extraction/mrtprocessor/bin/mrtprocessor --help
+
+
+ln -sf ~/bgp_research_tools/src/feature_extraction/mrtprocessor/repo/src/mrtprocessor/mrtprocessor ~/bgp_research_tools/src/feature_extraction/mrtprocessor/bin/mrtprocessor
+export DYLD_LIBRARY_PATH=~/bgp_research_tools/src/feature_extraction/mrtprocessor/lib
+~/bgp_research_tools/src/feature_extraction/mrtprocessor/bin/mrtprocessor --help
